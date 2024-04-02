f40c2c369f65825191534798af1e67e9b6994a8d
MathewsTanui <tanuimathews02@gmail.com>
2024-04-02 10:31:58 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    if (taskText.trim() !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+    }
+  });
 });
